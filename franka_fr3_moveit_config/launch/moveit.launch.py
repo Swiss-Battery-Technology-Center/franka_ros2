127ff11f5e202d58cf2fdd447fd289a49fafc395
sbtc-sipbb <sbtc@sipbb.ch>
2025-06-02 14:21:38 +0200

    made to work with cumotion

--- franka_fr3_moveit_config/launch/moveit.launch.py
+++ franka_fr3_moveit_config/launch/moveit.launch.py
@@ -100,22 +100,22 @@ def generate_launch_description():
     )
 
     # Planning Functionality
-    ompl_planning_pipeline_config = {
+    cumotion_planning_pipeline_config = {
         'move_group': {
-            'planning_plugin': 'ompl_interface/OMPLPlanner',
-            'request_adapters': 'default_planner_request_adapters/AddTimeOptimalParameterization '
-                                'default_planner_request_adapters/ResolveConstraintFrames '
-                                'default_planner_request_adapters/FixWorkspaceBounds '
-                                'default_planner_request_adapters/FixStartStateBounds '
-                                'default_planner_request_adapters/FixStartStateCollision '
-                                'default_planner_request_adapters/FixStartStatePathConstraints',
+            'planning_plugin': 'isaac_ros_cumotion_moveit/CumotionPlanner',
+            'request_adapters': ('default_planner_request_adapters/FixWorkspaceBounds'
+                                'default_planner_request_adapters/FixStartStateBounds'
+                                'default_planner_request_adapters/FixStartStateCollision'
+                                'default_planner_request_adapters/FixStartStatePathConstraints'),
             'start_state_max_bounds_error': 0.1,
+            'num_steps': 32,
         }
     }
-    ompl_planning_yaml = load_yaml(
-        'franka_fr3_moveit_config', 'config/ompl_planning.yaml'
+    cumotion_planning_yaml = load_yaml(
+        'franka_fr3_moveit_config', 'config/isaac_ros_cumotion_planning.yaml'
     )
-    ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
+    cumotion_planning_pipeline_config['move_group'].update(cumotion_planning_yaml)
+
 
     # Trajectory Execution Functionality
     moveit_simple_controllers_yaml = load_yaml(
@@ -150,7 +150,7 @@ def generate_launch_description():
             robot_description,
             robot_description_semantic,
             kinematics_yaml,
-            ompl_planning_pipeline_config,
+            cumotion_planning_pipeline_config,
             trajectory_execution,
             moveit_controllers,
             planning_scene_monitor_parameters,
@@ -171,7 +171,7 @@ def generate_launch_description():
         parameters=[
             robot_description,
             robot_description_semantic,
-            ompl_planning_pipeline_config,
+            cumotion_planning_pipeline_config,
             kinematics_yaml,
         ],
     )
